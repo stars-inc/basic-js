eb57a62d00b0fff65b918006a60db096b760d82c
stars_inc <stars_inc@mail.ru>
2021-03-06 22:52:09 +0300

    feat: feat something

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  const {repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'} = options
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  let first = [], second = []
+
+  const strArr = first.fill(str, repeatTimes)
+  const strDev = second.fill(addition, additionRepeatTimes).join(additionSeparator)
+
+  return strArr.map(el => el + strDev).join(separator)
+
+  // processing...
+}
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,9 @@
 module.exports = function transform(arr) {
   const comands = ['--discard-prev', '--double-prev', '--double-next', '--discard-next']
+
   if(!Array.isArray(arr)) throw new Error('not arr')
-  if(!arr.includes(comands)) return []
-  
-  return
+  if(!arr.includes(comands)) return arr
+
+  console.log(arr)
+
 }
