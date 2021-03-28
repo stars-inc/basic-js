7b346309955107bf62f4c62a6ad2729a02693e98
stars_inc <stars_inc@mail.ru>
2021-03-28 11:48:07 +0300

    feat: something

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,25 +1,32 @@
 class VigenereCipheringMachine {
-  constructor(mode = true) {
-    this.mode = mode
+  constructor(type = true) {
+    this.type = type
   }
    
   encrypt(text, key) {
     if(!text || !key) throw new Error()
+    let alphabet = [], i = 65, result = [], y = 0
 
-    let alphabet = [], i = 65
-
-    while(i <= 90) { // create alphebet
+    while(i <= 90) {
       alphabet.push(String.fromCodePoint(i))
       i++
     }
 
-    const getCodeFromPhrase = text.toUpperCase().split('').map(char => char.charCodeAt())
-    const getCodeFromKey = key.toUpperCase().split('').map(char => char.charCodeAt())
-    
-    console.log(alphabet, '==========>>>>>')
-    console.log(getCodeFromPhrase, 'Phrase CODE')
-    console.log(getCodeFromKey, 'KEY CODE')
+    const store = [text, key].map(char => char.toUpperCase().split(''))
+    const t = store[0], k = store[1] // destructurisation not allowed ;/
 
+    for(let i = 0; i < t.length; i++) {
+      if(alphabet.indexOf(t[i] < 0)) result.push(t[i])
+
+      let check = Math.abs(alphabet.indexOf(t[i]) + alphabet.indexOf(k[y]))
+
+      y++
+      if(check > alphabet.length) check = y 
+      
+      result.push(alphabet[check])
+    }
+
+    return !this.type ? result.join('') : result.reverse().join('')
   }
 
   decrypt(text, key) {
